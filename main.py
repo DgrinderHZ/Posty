8ff5ae4c2c1b698a949e5c031ac93baf1af58992
DgrinderHZ <hassanzekkouri@gmail.com>
2020-04-28 17:10:33 +0000

    Initial commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ dummy_posts = [
     Post(id=1,
          photo_url='https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=50&w=50',
          name='Sara',
-         body='Lorem Ipsum'),
+         body='Hi, it is my first post'),
     Post(id=2,
          photo_url='https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=1&h=100&w=100',
          name='John',
